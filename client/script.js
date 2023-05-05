bd58a542a724152f4d585b36f0de150f0cf9d7cd
Shanmukh Ram <shanmukhram@Shanmukhs-MacBook-Air.local>
2023-05-05 13:17:31 +0530

    first commit

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5017', {
+    const response = await fetch('https://codex-c8aa.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
